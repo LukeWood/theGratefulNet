2efc89b4f44e4c2634f8e5c1d6c460d42e916441
Luke Wood <lswood@smu.edu>
2016-10-25 16:34:10 -0500

    data handler looks perfect

--- learning/main.py
+++ learning/main.py
@@ -2,5 +2,4 @@ from prepare_data import data_manager
 
 dm = data_manager()
 dm.add_data("files.txt")
-dm.remove_uncommon()
-print(dm.get_training_data())
+print(dm.get_raw_data())
--- learning/prepare_data.py
+++ learning/prepare_data.py
@@ -12,14 +12,14 @@ class data_manager:
         self.parsed_sentences = []
 
     def add_data(self,f0):
-        self.sentences = self.sentences + self.tokenize(["%s %s %s" % (self.sentence_start_token, x, self.sentence_end_token) for x in open(f0,"r")])
-        self.remove_uncommon()
+        self.sentences = self.sentences + self.__tokenize(["%s %s %s" % (self.sentence_start_token, x, self.sentence_end_token) for x in open(f0,"r")])
+        self.__remove_uncommon()
 
-    def tokenize(self,sentences):
+    def __tokenize(self,sentences):
         return [nltk.word_tokenize(sentence) for sentence in sentences]
 
     #Only call this after we load all data
-    def remove_uncommon(self):
+    def __remove_uncommon(self):
         word_freq = nltk.FreqDist(itertools.chain(*self.sentences))
         words = word_freq.most_common(self.vocabulary_size-1)
         self.index_to_word= [x[0] for x in words]
