57c6db0bb558c1bfcb475e7a7656fe9cf42e8724
Luke Wood <lswood@smu.edu>
2016-10-29 17:42:25 -0500

    createmodel

--- main/create_model.py
+++ main/create_model.py
@@ -14,10 +14,15 @@ word_to_index, index_to_word = dm.get_indices()
 
 model = RNN(word_to_index,index_to_word,word_dim = vocab_size)
 
-model.load("models/model.data.npz")
+x_train, y_train = dm.get_training_data()
+# This is with a random loss
+print("Expected loss: %f" % (np.log(vocab_size)))
+print("Actual loss: %f" % (model.calculate_loss(x_train[:1000],y_train[:1000])))
 
-min_length = 4
-sentence = []
+model.train_with_sgd(x_train,y_train,nepoch=100, learning_rate=.005)
+
+model.save("models/model.data")
 
 sentence = model.create_sentence()
+
 print(" ".join(sentence))
