ea4b651f5a9cfd75c7ba170dc883ff91ea5e3d3e
Luke Wood <lswood@smu.edu>
2016-10-29 15:38:07 -0500

    rnn theano working

--- main/RNN.py
+++ main/RNN.py
@@ -1,106 +1,145 @@
 
 # Note! The basic structure of this neural network was primarily created using a tutorial found in the credits
 # This is primarily a research project and I take no claim to the thought process behind the code below.
-# I have only (as of now) made minor changes to the code.
+# I did change the code a lot to make it object oriented and a bit easier to follow as well as ported it to python 3
 
 import numpy as np
-from numpy import exp
+import theano as theano
+import theano.tensor as T
+from utils import *
+import operator
+from datetime import datetime
+import sys
+
+
 class RNN:
-    def __init__(self, word_dim=8000, hidden_dim=100, bptt_depth=4):
+
+    def __init__(self, word_dim, hidden_dim=100, bptt_truncate=4):
+        # Assign instance variables
         self.word_dim = word_dim
         self.hidden_dim = hidden_dim
-        self.bptt_depth = bptt_depth
+        self.bptt_truncate = bptt_truncate
+        # Randomly initialize the network parameters
+        U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
+        V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
+        W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+        # Theano: Created shared variables
+        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))
+        # We store the Theano graph here
+        self.theano = {}
+        self.__theano_build__()
+        self.num_examples_seen = 0
 
-        #Network parameters begin randomly initialized
-        self.U = np.random.uniform(-np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
-        self.V = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
-        self.W = np.random.uniform(-np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+    def __theano_build__(self):
+        U, V, W = self.U, self.V, self.W
+        x = T.ivector('x')
+        y = T.ivector('y')
+        def forward_prop_step(x_t, s_t_prev, U, V, W):
+            s_t = T.tanh(U[:,x_t] + W.dot(s_t_prev))
+            o_t = T.nnet.softmax(V.dot(s_t))
+            return [o_t[0], s_t]
+        [o,s], updates = theano.scan(
+            forward_prop_step,
+            sequences=x,
+            outputs_info=[None, dict(initial=T.zeros(self.hidden_dim))],
+            non_sequences=[U, V, W],
+            truncate_gradient=self.bptt_truncate,
+            strict=True)
 
-    # This is just a fancy word for feeding through the network to get output
-    def forward_propogation(self, x):
-        steps_total = len(x)
+        prediction = T.argmax(o, axis=1)
+        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
 
-        # We save inside of here so that we will need to recover them after we use them later
-        s = np.zeros((steps_total+1,self.hidden_dim))
-        s[-1] = np.zeros(self.hidden_dim)
+        # Gradients
+        dU = T.grad(o_error, U)
+        dV = T.grad(o_error, V)
+        dW = T.grad(o_error, W)
 
-        o = np.zeros((steps_total, self.word_dim))
+        # Assign functions
+        self.forward_propagation = theano.function([x], o)
+        self.predict = theano.function([x], prediction)
+        self.ce_error = theano.function([x, y], o_error)
+        self.bptt = theano.function([x, y], [dU, dV, dW])
 
-        for i in np.arange(steps_total):
-            #Numpy .dot is actually matrix multiplication not dot product.
+        # SGD
+        learning_rate = T.scalar('learning_rate')
+        self.sgd_step = theano.function([x,y,learning_rate], [],
+                      updates=[(self.U, self.U - learning_rate * dU),
+                              (self.V, self.V - learning_rate * dV),
+                              (self.W, self.W - learning_rate * dW)])
 
-            v1 = s[i-1]
+    def calculate_total_loss(self, X, Y):
+        return np.sum([self.ce_error(x,y) for x,y in zip(X,Y)])
 
-            #self.W = 100x100
+    def calculate_loss(self, X, Y):
+        # Divide calculate_loss by the number of words
+        num_words = np.sum([len(y) for y in Y])
+        return self.calculate_total_loss(X,Y)/float(num_words)
 
-            v2 = self.W.dot(v1)
+    def train_with_sgd(self, X_train, y_train, learning_rate=0.005, nepoch=1, evaluate_loss_after=5):
+        # We keep track of the losses so we can plot them later
+        losses = []
+        num_examples_seen = 0
+        for epoch in range(nepoch):
+            # Optionally evaluate the loss
+            if (epoch % evaluate_loss_after == 0):
+                loss = self.calculate_loss(X_train, y_train)
+                losses.append((num_examples_seen, loss))
+                time = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+                print("%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, loss) )
+                # Adjust the learning rate if loss increases
+                if (len(losses) > 1 and losses[-1][1] > losses[-2][1]):
+                    learning_rate = learning_rate * 0.5
+                    print ("Setting learning rate to %f" % learning_rate)
+                sys.stdout.flush()
+            # For each training example...
+            for i in range(len(y_train)):
+                # One SGD step
+                self.sgd_step(X_train[i], y_train[i], learning_rate)
+            self.num_examples_seen += 1
 
-            v3 = self.U[:,x[i]]
-
-            s[i] = np.tanh(v3 + v2)
-            o[i] = self.softmax(self.V.dot(s[i]))
-
-        return [o, s]
-
-    # loss function represents total errors
-    def calculate_total_loss(self, x, y):
-        L = 0
-        for i in np.arange(len(y)):
-            o, s = self.forward_propogation(x[i])
-            correct_words = o[np.arange(len(y[i])), y[i]]
-
-            L+= -1 * np.sum(np.log(correct_words))
-        return L
-
-    def calculate_loss(self, x, y):
-        N = np.sum((len(y_i) for y_i in y))
-        return self.calculate_total_loss(x,y)/N
-
-    # this just returns softmax
-
-    def softmax(self,w, t = 1.0):
-        e = np.exp(np.array(w) / t)
-        dist = e / np.sum(e)
-        return dist
-
-    # Take the most likely outcome, our RNN only creates probabilities.  We simply take the best
-    def predict(self, x):
-        o, s = self.forward_propogation(x)
-        return np.argmax(o, axis=1)
-
-    #I'm defining this instead of using numpy's implementation so that we can try different nonlinear functions more easily.
-    def nonlin(x):
-        #tanh(x) = (e^x - e^-x)/(e^x +e^-x)
-        #np.exp = e^x
-        return (exp(x) - exp(-x))/(np.exp(x) + np.exp(-x))
-
-
-    def bptt(self, x, y):
-        T = len(y)
-
-        o,s = self.forward_propogation(x)
-
-        dLdU = np.zeros(self.U.shape)
-        dLdV = np.zeros(self.V.shape)
-        dLdW = np.zeros(self.W.shape)
-
-        delta_o = o
-        delta_o[np.arange(len(y)),y] -= 1
-
-        for t in np.arange(T)[::-1]:
-            dLdV = np.outer(delta_o[t], s[t].T)
-
-            delta_t = self.V.T.dot(delta_o[t]) * (1 - (s[t] **2))
-
-            for bptt_step in np.arange(max(0, t-self.bptt_depth),t+1)[::-1]:
-                dLdW += np.outer(delta_t, s[bptt_step-1])
-                dLdU[:,x[bptt_step]] += delta_t
-                delta_t = self.W.T.dot(delta_t) * (1 - s[bptt_step-1] ** 2)
-        return [dLdU, dLdV, dLdW]
-
-    def train(self,x,y,iterations = 1000):
-        for i in range(iterations):
-            du,dv,dw = self.bptt(x,y)
-            self.U += du
-            self.V +=dv
-            self.W+=dw
+    def gradient_check_theano(self, x, y, h=0.001, error_threshold=0.01):
+        # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
+        self.bptt_truncate = 1000
+        # Calculate the gradients using backprop
+        bptt_gradients = self.bptt(x, y)
+        # List of all parameters we want to chec.
+        model_parameters = ['U', 'V', 'W']
+        # Gradient check for each parameter
+        for pidx, pname in enumerate(model_parameters):
+            # Get the actual parameter value from the mode, e.g. model.W
+            parameter_T = operator.attrgetter(pname)(self)
+            parameter = parameter_T.get_value()
+            print ("Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape)))
+            # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+            it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+            while not it.finished:
+                ix = it.multi_index
+                # Save the original value so we can reset it later
+                original_value = parameter[ix]
+                # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+                parameter[ix] = original_value + h
+                parameter_T.set_value(parameter)
+                gradplus = self.calculate_total_loss([x],[y])
+                parameter[ix] = original_value - h
+                parameter_T.set_value(parameter)
+                gradminus = self.calculate_total_loss([x],[y])
+                estimated_gradient = (gradplus - gradminus)/(2*h)
+                parameter[ix] = original_value
+                parameter_T.set_value(parameter)
+                # The gradient for this parameter calculated using backpropagation
+                backprop_gradient = bptt_gradients[pidx][ix]
+                # calculate The relative error: (|x - y|/(|x| + |y|))
+                relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+                # If the error is to large fail the gradient check
+                if relative_error > error_threshold:
+                    print("Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix))
+                    print("+h Loss: %f" % gradplus)
+                    print("-h Loss: %f" % gradminus)
+                    print("Estimated_gradient: %f" % estimated_gradient)
+                    print("Backpropagation gradient: %f" % backprop_gradient)
+                    print("Relative Error: %f" % relative_error)
+                    return
+                it.iternext()
+                print("Gradient check for parameter %s passed." % (pname))
--- main/main.py
+++ main/main.py
@@ -3,7 +3,7 @@ from glob import glob
 from RNN import RNN
 import numpy as np
 
-vocab_size = 2575
+vocab_size = 50
 
 dm = data_manager(vocab_size=vocab_size)
 
@@ -16,6 +16,5 @@ x_train, y_train = dm.get_training_data()
 # This is with a random loss
 print("Expected loss: %f" % (np.log(vocab_size)))
 print("Actual loss: %f" % (model.calculate_loss(x_train[:1000],y_train[:1000])))
-for x, y in zip(x_train, y_train):
-    model.train(x,y)
-    print("Actual loss: %f" % (model.calculate_loss(x_train[:1000],y_train[:1000])))
+while(True):
+    model.train_with_sgd(x_train,y_train)
