5f7b82e432975c672a55f06a0cbbccb9b983aaf8
Luke Wood <lswood@smu.edu>
2016-10-30 17:23:20 -0500

    punctuation

--- docs/index.js
+++ docs/index.js
@@ -7,7 +7,10 @@ $.getJSON("song2.json",function (data){
 function run(data){
   var song = document.getElementById("song");
   var lines = data.text.split("</br>");
-  var i = 0;
+  var i = 1;
+  var title = document.createElement("h2");
+  title.innerHTML = lines[0];
+  document.getElementById("cen").appendChild(title);
   (function startLine(){
     var line = document.createElement("p");
     line.className = "line";
