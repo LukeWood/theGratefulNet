8400fe2f1c2295ca7ef64d1dd14cdac7ad879e41
Luke Wood <lswood@smu.edu>
2016-11-11 19:55:40 -0600

    a

--- main/train_model.py
+++ main/train_model.py
@@ -16,8 +16,10 @@ model = RNN(word_to_index,index_to_word,word_dim = vocab_size)
 
 x_train, y_train = dm.get_training_data()
 # This is with a random loss
+print("Expected loss: %f" % (np.log(vocab_size)))
+print("Actual loss: %f" % (model.calculate_loss(x_train[:1000],y_train[:1000])))
 
-model.train_with_sgd(x_train,y_train,nepoch=1000, learning_rate=.005)
+model.train_with_sgd(x_train,y_train,nepoch=100, learning_rate=.005)
 
 model.save("models/model.data")
 
