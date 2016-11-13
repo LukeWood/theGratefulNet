82247f11ab1e502aa07e82f95d746bc30cb831bc
Luke Wood <lswood@smu.edu>
2016-11-13 17:19:33 -0600

    shakin

--- docs/index.js
+++ docs/index.js
@@ -6,6 +6,10 @@ $.getJSON("song2.json",function (data){
     run(data,document.getElementById("innercontainer2"),document.getElementById("song2title"));
 });
 
+$.getJSON("song3.json",function(data){
+  run(data,document.getElementById("innercontainer3"),document.getElementById("song3title"));
+});
+
 if(typeof(String.prototype.trim) === "undefined")
 {
     String.prototype.trim = function()
@@ -25,12 +29,16 @@ function capitolize(str){
 function run(data,dom_element,title_div){
   var song = dom_element;
   var lines = data.text.split("</br>");
+  console.log(lines);
   var title = document.createElement("h2");
   title.innerHTML = lines[0]+"</br>";
   title.setAttribute("style","font-family: 'Calligraffitti', cursive; font-size:35px;");
   title_div.appendChild(title);
 
   (function startLine(i){
+    if(i >= lines.length){
+      return;
+    }
     var line = document.createElement("p");
     line.className = "line";
     line.innerHTML = capitolize(lines[i]);
@@ -38,9 +46,7 @@ function run(data,dom_element,title_div){
     if(line.innerHTML.length > 1){
       song.appendChild(line);
     }
-    if(i == lines.length){
-      return;
-    }
+
     startLine(i+1);
   }
 )(1);
